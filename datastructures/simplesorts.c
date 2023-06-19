c9d4474d690381984ca91cfb7c5103e548bc34ec
Jamie Barber <jamie.aba@gmail.com>
2023-06-19 08:04:21 -0500

    Selection Sort in C started, no sort yet

--- datastructures/simplesorts.c
+++ datastructures/simplesorts.c
@@ -31,8 +31,23 @@ int *allocate(int numofelements){
     return(p);
 }
 
-/*The efficiency of bubble sort
-The idea is */
+
+/*Bubble Sort:
+Two pointers will point to the first element and the
+second element in an array, 
+
+the two elements will be compared, 
+
+if one is smaller than the other, the elements are swapped, 
+
+if not, both pointers are incremented and the next two elements are compared
+
+this is done until every element in the array has been compared
+
+if no swaps are needed for an entire for loop (pass-through), the array is sorted
+
+Worst-case Efficiency: N**2, for 5 elements 10 comparisons, 10 swaps */
+
 void bubble_sort(int *array){
     int *first;
     int *next;
@@ -56,7 +71,47 @@ void bubble_sort(int *array){
         
         }
     }
+
+    free(first);
+    free(next);
 }
+
+void selection_sort(int* array){
+int *first;
+int smallest;
+int smallest_index;
+int swap_num = 0;
+int unsorted = 1;
+
+first = allocate(1);
+
+
+
+
+while(unsorted){
+
+    first = array;     /*address of array[0]*/
+    smallest = *array; /*array[0]*/
+    unsorted = 0;
+
+
+    for(int i = 0; i < SIZE; i++){
+        if(*first < smallest){
+            smallest = *first;
+            smallest_index = i;
+            swap_num = 1;
+            unsorted = 1;
+        }
+    }
+    if(swap_num){
+        swap(first,(array+smallest_index));
+        swap_num = 0;
+    }
+    (first++);
+}
+    
+}
+
 
 int main()
 {
@@ -74,7 +129,10 @@ int main()
    
 
     /*Bubble Sort*/
-    bubble_sort(arrayofnum);
+    /*bubble_sort(arrayofnum);*/
+
+    /*Selection Sort*/
+    selection_sort(arrayofnum);
 
     for(int i = 0; i < SIZE; i++){
         printf("%d, ", arrayofnum[i]);
